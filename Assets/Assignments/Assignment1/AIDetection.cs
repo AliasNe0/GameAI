97ef9a22c38e9ce3bb03bfe86e7e34fa82f26200
Ilia Chichkanov <ilia.chichkanov@gmail.com>
2023-07-22 20:12:33 +0300

    Detection debugger works

--- Assets/Assignments/Assignment1/AIDetection.cs
+++ Assets/Assignments/Assignment1/AIDetection.cs
@@ -7,28 +7,29 @@ namespace ASSIGNMENT1
 {
     public class AIDetection : MonoBehaviour
     {
-        [SerializeField]
-        float distanceRange = 10f;
-        [SerializeField]
-        float rays = 10f;
-        [SerializeField]
-        float angleRange = 180f;
+        [SerializeField] int rays = 25;
+        [SerializeField] float rayOriginHeight = 1.5f;
+        [SerializeField] float rayEndHeight = 0.2f;
+        [SerializeField] float distanceRange = 7f;
+        [SerializeField] float angleRange = 120f;
 
         void FixedUpdate()
         {
+            Vector3 rayOrigin = transform.position + new Vector3(0, rayOriginHeight, 0);
+            float yRotationNormalized = (rayEndHeight - rayOriginHeight) / distanceRange;
+            LayerMask collectableLayerMask = LayerMask.GetMask("Collectable");
             for (int i = 0; i <= rays; i++)
             {
                 float rayFactor = 2f * i / rays - 1f;
                 float rayAngle = angleRange * Mathf.Deg2Rad / 2 * rayFactor;
-                Vector3 rayRotation = new Vector3(Mathf.Sin(rayAngle), 0, Mathf.Cos(rayAngle));
-                Vector3 rayEndPosition = transform.position + rayRotation * distanceRange;
-                Debug.DrawLine(transform.position, rayEndPosition, Color.red);
+                Vector3 rayRotation = Vector3.Normalize(new Vector3(Mathf.Sin(rayAngle), yRotationNormalized, Mathf.Cos(rayAngle)));
+                Vector3 rayEndPosition = rayOrigin + rayRotation * distanceRange;
+                Debug.DrawLine(rayOrigin, rayEndPosition, Color.red);
                 RaycastHit hit;
-                if (Physics.Raycast(transform.position, rayEndPosition, out hit))
+                if (Physics.Raycast(rayOrigin, rayRotation, out hit, distanceRange, collectableLayerMask))
                 {
-                    Debug.DrawLine(transform.position, rayEndPosition, Color.green);
+                    Debug.DrawLine(rayOrigin, rayEndPosition, Color.green);
                 }
-
             }
         }
     }
