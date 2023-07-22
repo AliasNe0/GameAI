3cddad4fe954c03f763923109c55a2220d331d0f
Ilia Chichkanov <ilia.chichkanov@gmail.com>
2023-07-23 00:33:57 +0300

    Update AISteering.cs

--- Assets/Assignments/Assignment1/AISteering.cs
+++ Assets/Assignments/Assignment1/AISteering.cs
@@ -7,16 +7,33 @@ namespace ASSIGNMENT1
 {
     public class AISteering : MonoBehaviour
     {
-        [SerializeField] GameObject target;
         [SerializeField] float speed = 2f;
+
+        GameObject bait;
+
+        private void Awake()
+        {
+            bait = Instantiate(new GameObject("Bait"));
+            bait.transform.position = transform.position + transform.forward * 3f;
+            bait.transform.parent = transform;
+        }
 
         void FixedUpdate()
         {
-            if (target)
-            {
-                Vector3 direction = Vector3.Normalize(target.transform.position - transform.position);
-                transform.position += direction * speed * Time.deltaTime;
+            if (bait)
+            {                
+                Vector3 direction = Vector3.Normalize(bait.transform.position - transform.position);
+                transform.position += speed * Time.deltaTime * direction;
                 transform.rotation = Quaternion.LookRotation(direction);
+            }
+        }
+
+        void OnDrawGizmos()
+        {
+            if (bait)
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawWireSphere(bait.transform.position + bait.transform.up, 0.25f);
             }
         }
     }
