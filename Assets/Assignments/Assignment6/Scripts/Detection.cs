067a5b1505de0505c3c766ecc2e5c468be09b804
Ilia Chichkanov <ilia.chichkanov@gmail.com>
2023-08-05 13:11:34 +0300

    Small fix to assignment 6

--- Assets/Assignments/Assignment6/Scripts/Detection.cs
+++ Assets/Assignments/Assignment6/Scripts/Detection.cs
@@ -7,21 +7,29 @@ namespace ASSIGNMENT6
 {
     public class Detection : MonoBehaviour
     {
-        [SerializeField] float detectionRange = 10f;
+        [SerializeField] float minDetectionDistance = 6.5f;
 
         public GameObject Ball { get; private set; }
+
+        float detectionDistance = 0;
+
+        private void Awake()
+        {
+            detectionDistance = minDetectionDistance;
+        }
 
         void Update()
         {
             if (Ball == null)
             {
                 GameObject ball = GameObject.FindGameObjectWithTag("Ball");
-                if (ball && Vector3.Distance(ball.transform.position, transform.position) < detectionRange)
+                if (ball && Vector3.Distance(ball.transform.position, transform.position) < detectionDistance)
                 {
+                    detectionDistance = Random.Range(minDetectionDistance, 13f);
                     Ball = ball;
                 }
             }
-            else if (Vector3.Distance(Ball.transform.position, transform.position) > detectionRange)
+            else if (Vector3.Distance(Ball.transform.position, transform.position) > minDetectionDistance)
             {
                 Ball = null;
             }
